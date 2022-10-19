7e765bcc310f528b835fed21e0bd9c3d7344c789
Harun Dastekin <harun.dastekin@student.htw-berlin.de>
2022-10-19 20:41:38 +0200

    added product pojo

--- src/main/java/kbe/hadebe/uebung1/Product.java
+++ src/main/java/kbe/hadebe/uebung1/Product.java
@@ -1,2 +1,53 @@
-package kbe.hadebe.uebung1;public class Product {
+package kbe.hadebe.uebung1;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Product {
+
+
+
+    @Getter
+    @Setter
+    private int id;
+
+
+    @Getter
+    @Setter
+    private String product_nr;
+
+    @Getter
+    @Setter
+    private String productName;
+    @Getter
+    @Setter
+    private int price;
+    @Getter
+    @Setter
+    private Boolean hasSDSlot;
+
+    @Getter
+    @Setter
+    private String name_manufacturer;
+
+    @Getter
+    @Setter
+    private String ean;
+
+
+    public Product(int id, String product_nr, String productName, int price, Boolean hasSDSlot, String name_manufacturer, String ean) {
+        this.id = id;
+        this.product_nr = product_nr;
+        this.productName = productName;
+        this.price = price;
+        this.hasSDSlot = hasSDSlot;
+        this.name_manufacturer = name_manufacturer;
+        this.ean = ean;
+    }
+
+    public Product() {
+
+    }
+
+
 }
--- src/main/java/kbe/hadebe/uebung1/ProductController.java
+++ src/main/java/kbe/hadebe/uebung1/ProductController.java
@@ -1,2 +1,15 @@
-package kbe.hadebe.uebung1;public class ProductController {
+package kbe.hadebe.uebung1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+
+    //@GetMapping("/products")
+
+
+    //@GetMapping("/products/{PRODUCT_ID}")
+
 }
